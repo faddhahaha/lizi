ea5c62c0acbacc3f1ff3fc08ce1f837530e0205e
faddhahaha <1391167617@qq.com>
2021-08-26 14:37:37 +0800

    test

--- 08/src/pages/App.js
+++ 08/src/pages/App.js
@@ -8,7 +8,7 @@ class App extends Component {
         super(props);
         this.state={navigationValue:location.hash.substring(1)}
     }
-    onSwich = (value) =>{
+    onSwich = (value) =>
         this.setState({
             navigationValue:value
         })
